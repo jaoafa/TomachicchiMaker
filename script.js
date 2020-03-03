e49c80a46e4c853e2bcc57c71814d932b3ee2232
Tomachi (ICHIGO) <lego@aol.jp>
2020-03-04 02:25:03 +0900

    fix: cloudflareのonclickとかの仕様回避

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+boot();
+
+function boot() {
+    reset();
+
+    for (let colbutton of document.getElementsByClassName("color_buttons")) {
+        colbutton.addEventListener("keyup", function () {
+            changeTomachi(colbutton)
+        });
+        colbutton.addEventListener("change", function () {
+            changeTomachi(colbutton)
+        });
+    }
+    document.getElementById("resetButton").addEventListener("click", function () {
+        reset()
+    });
+}
+
 function changeTomachi(e) {
     document.getElementById(e.dataset.pathid).style.fill = e.value;
 }
